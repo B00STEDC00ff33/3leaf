8d6cdcabedde42a85a81df465460945d38aef411
SooYeon <41eaf.pyt@gmail.com>
2023-01-24 18:16:28 +0900

    brush up

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ document.addEventListener('scroll', () => {
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-
     const target = event.target;
     const link = target.dataset.link;
     if (link == null) {
@@ -30,7 +29,7 @@ navbarMenu.addEventListener('click', (event) => {
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 navbarToggleBtn.addEventListener('click', () => {
     navbarMenu.classList.toggle('open');
-})
+});
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
@@ -58,7 +57,7 @@ document.addEventListener('scroll', () => {
 //Handle click on the "arrow up"button
 arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
-})
+});
 
 //Projects
 const workBtnContainer = document.querySelector('.work__categories');
@@ -72,8 +71,9 @@ workBtnContainer.addEventListener('click', (e) => {
 
 // Remove selection from the previous item and select the new one
 const active = document.querySelector('.category__btn.selected');
-active.classList.remove('selected');
-const target = e.target.nodeName === 'BUTTON' ? e.target :
+if (active != null) {
+    active.classList.remove('selected');
+}
 e.target.classList.add('selected');
 
     projectContainer.classList.add('anim-out');
