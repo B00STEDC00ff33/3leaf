526d1745120b3d7cc98f7e698bbd14ea9cd8ee1c
SooYeon <41eaf.pyt@gmail.com>
2023-01-24 14:19:02 +0900

    JS arrow-up

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     }
+
     scrollIntoView(link);
 });  
 
@@ -28,6 +29,29 @@ const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
+
+//Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+//show "arrow up"button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+//Handle click on the "arrow up"button
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+})
+
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
